438cee3615067b276313803552a2dedf47706a5c
James Cox <jamesncox@yahoo.com>
2019-07-24 14:02:30 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,7 +12,7 @@ class SchoolClassesController < ApplicationController
   end 
   
   def create 
-    @school_class = SchoolClass.new(school_class_params(:title, :room_number)) 
+    @school_class = SchoolClass.new(school_class_params) 
     @school_class.save
     redirect_to school_class_path(@school_path)
   end
@@ -23,15 +23,15 @@ class SchoolClassesController < ApplicationController
   
   def update 
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(school_class_params(:title, :room_number))
+    @school_class.update(school_class_params)
     redirect_to school_class_path(@school_path)
   end 
   
   private
   
-  def school_class_params(*args)
+  def school_class_params
     binding.pry
-    params.require(:school_class).permit(*args)
+    params.require(:school_class).permit(:title, :room_number)
   end 
   
 end
